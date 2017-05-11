9f0d004a846a0501854b5247745d6bb2efc29762
Dirk Soentjens <dsoentjens@trivago.com>
2017-05-11 15:56:08 +0200

    added a shot folder and removed unnessesary comments

--- selenium.rb
+++ selenium.rb
@@ -15,7 +15,6 @@ languages.each {  |language|
   #make directory/ folder
   Dir.mkdir 'shots/'+ language unless File.exists?('shots/'+ language)
   screenshot_path = 'shots/'+ language+'/'
-  #language_no_dots = language.sub '.', '_'
   wait = Selenium::WebDriver::Wait.new(:timeout => 30)
   driver.navigate.to 'https://www.trivago.'+language+'/hotelmanager/login.html'
 
@@ -39,21 +38,6 @@ languages.each {  |language|
       element if element.displayed?
   }
   puts " - we are logged into hotel manager for " + language if input.displayed?
-
-  # driver.navigate.to 'https://www.trivago.'+language+'/hotelmanager/rates/3854016/rate_shopper.html'
-  # driver.execute_script("document.body.style['zoom'] = 0.9; $('.leftNavigation-wrapper').remove(); $('.logo-navi-wrapper').remove(); $('.mainNavigation').remove(); $('footer').remove(); $('.alert--info').remove(); $('.header-unified').css({'padding-top':0})");
-  # sleep(1)
-  # driver.manage.window.resize_to(1600, 1343)
-  # content = wait.until {
-  #     element = driver.find_element(:id, "hgw_main_content")
-  #     element if element.displayed?
-  # }
-  # puts "we are in rate shoper" if content.displayed?
-  # sleep(1)
-  # driver.save_screenshot(screenshot_path+'rate_benchmarking.png')
-  # image = MiniMagick::Image.open( screenshot_path+'rate_benchmarking.png')
-  # image.crop "1160x1340+0+0"
-  # image.write screenshot_path +'rate_benchmarking.png'
 
   driver.navigate.to 'https://www.trivago.'+language+'/hotelmanager/hotel_news/3854016/hotel_news.html'
   driver.execute_script("$('.leftNavigation-wrapper').remove(); $('.logo-navi-wrapper').remove(); $('.mainNavigation').remove(); $('footer').remove(); $('#alertHotelNews').remove(); $('.header-unified').css({'padding-top':0}); $('#hgw_main_content').css({'padding-top':0});")
@@ -95,21 +79,6 @@ languages.each {  |language|
   puts " - updated hotel_news.png on " + server_2
   #puts files
   ftp.close
-
-# driver.navigate.to 'https://www.trivago.'+language+'/hotelmanager/customer_profile/3854016/customer_profile.html'
-#   driver.execute_script("document.body.style['zoom'] = 0.69; $('.leftNavigation-wrapper').remove(); $('.logo-navi-wrapper').remove(); $('.mainNavigation').remove(); $('footer').remove(); $('.alert--info').remove(); $('.header-unified').css({'padding-top':0}); $('#hgw_main_content').css({'padding-top':0});")
-#   content = wait.until {
-#       element = driver.find_element(:id, "hgw_main_content")
-#       element if element.displayed?
-#   }
-#   puts "we are in customer_profile" if content.displayed?
-#   driver.manage.window.resize_to(1280, 1050)
-#
-#   driver.save_screenshot(screenshot_path+'visitors_profiles.png')
-#
-#   image = MiniMagick::Image.open(screenshot_path+'visitors_profiles.png')
-#   image.crop "869x936+25+0"
-#   image.write screenshot_path+'visitors_profiles.png'
   puts " -------- end  "+ language +"  -------- "
   puts 
 }
